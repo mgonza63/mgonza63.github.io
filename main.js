43b23f7f87a02e832bb65109b8c024f9252ff879
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-02-12 23:22:42 -0600

    added bootstrap and main js with parallax functions

--- main.js
+++ main.js
@@ -1,33 +1,28 @@
-// const first = document.getElementById("first");
-// const third = document.getElementById("third");
+const first = document.getElementById("first");
+const third = document.getElementById("third");
 
 
 
-// const parallaxY = (element, distance, speed) => {
-//     const item = document.getElementById(element);
-//     item.style.transform = `translateY(${distance * speed}px)`;
-// }
-// const parallaxX = (element, distance, speed) => {
-//     const item = document.getElementById(element);
-//     item.style.transform = `translateX(${distance * speed}px)`;
-// }
+const parallaxY = (element, distance, speed) => {
+     const item = document.getElementById(element);
+     item.style.transform = `translateY(${distance * speed}px)`;
+ }
+const parallaxX = (element, distance, speed) => {
+     const item = document.getElementById(element);
+     item.style.transform = `translateX(${distance * speed}px)`;
+ }
 
-// window.addEventListener("scroll", function() {
-//     parallaxY("first-title", window.scrollY, 0.3);
-//     parallaxY("first-subtitle", window.scrollY, -0.3);
+ window.addEventListener("scroll", function() {
+     parallaxY("first-title", window.scrollY, 0.3);
+     parallaxY("first-subtitle", window.scrollY, -0.3);
+     
+     parallaxY("austin-col", window.scrollY, -0.3);
 
-//     parallaxY("me-pic", window.scrollY, -0.5);
-//     parallaxX("me-pic", window.scrollY, 0.6);
+     parallaxY("me-col", window.scrollY, 0.4);
+     
+     parallaxY("second-title-col", window.scrollY, -0.9 );
 
-//     parallaxY("austin-pic", window.scrollY, -0.7);
-//     parallaxX("austin-pic", window.scrollY, -0.2);
-// });
+ });
 
-var images = document.getElementsByClassName('austin'); 
-
-new simpleParallax(images, {
-    scale: 1.5,
-	orientation: 'right'
-});
 
 
