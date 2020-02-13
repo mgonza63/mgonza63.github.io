d176671d48a26555d02f939278ac5bde6707f9e1
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-02-12 21:13:47 -0600

    added background picutre and simple parallax js

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
-const first = document.getElementById("first");
-const third = document.getElementById("third");
+// const first = document.getElementById("first");
+// const third = document.getElementById("third");
 
-window.addEventListener("scroll", function () {
-    let offset = window.pageYOffset;
-    
-    first.style.backgroundPositionY = offset * 1.05 + "px";
-})
+
+
+// const parallaxY = (element, distance, speed) => {
+//     const item = document.getElementById(element);
+//     item.style.transform = `translateY(${distance * speed}px)`;
+// }
+// const parallaxX = (element, distance, speed) => {
+//     const item = document.getElementById(element);
+//     item.style.transform = `translateX(${distance * speed}px)`;
+// }
+
+// window.addEventListener("scroll", function() {
+//     parallaxY("first-title", window.scrollY, 0.3);
+//     parallaxY("first-subtitle", window.scrollY, -0.3);
+
+//     parallaxY("me-pic", window.scrollY, -0.5);
+//     parallaxX("me-pic", window.scrollY, 0.6);
+
+//     parallaxY("austin-pic", window.scrollY, -0.7);
+//     parallaxX("austin-pic", window.scrollY, -0.2);
+// });
+
+var images = document.getElementsByClassName('austin'); 
+
+new simpleParallax(images, {
+    scale: 1.5,
+	orientation: 'right'
+});
+
+
